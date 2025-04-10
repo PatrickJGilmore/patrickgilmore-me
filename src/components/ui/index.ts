0a7fb9e2045c1ac933349420b39acba7bd899efa
PatrickJGilmore <63202765+PatrickJGilmore@users.noreply.github.com>
2025-04-09 22:21:50 -0400

    Create index.ts

---
+++ src/components/ui/index.ts
@@ -0,0 +1,4 @@
+// src/components/ui/index.ts
+export { default as Button } from "./Button";
+export { default as Nav } from "./Nav";
+// Add similar export lines for any other components in this folder…
