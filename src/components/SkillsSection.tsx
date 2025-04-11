ead49321b28ee6b5059edef7588a643d7338dddf
PatrickJGilmore <63202765+PatrickJGilmore@users.noreply.github.com>
2025-04-11 02:43:13 -0400

    Update SkillsSection.tsx

--- src/components/SkillsSection.tsx
+++ src/components/SkillsSection.tsx
@@ -103,7 +103,7 @@ const SkillsSection = () => {
         { name: "Requirements Analysis" },
         { name: "GitHub", url: "https://github.com/" },
         { name: "Postman/API Testing", url: "https://www.postman.com/" },
-        { name: "Visio", url: "https://www.microsoft.com/en-us/microsoft-365/visio/flowchart-software" },
+        { name: "JavaScript", url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript" },
         { name: "Aha", url: "https://www.aha.io/" },
         { name: "HTML", url: "https://developer.mozilla.org/en-US/docs/Web/HTML" },
         { name: "CSS", url: "https://developer.mozilla.org/en-US/docs/Web/CSS" }
