81a732c1c354b0a02f6f1e9970f4625366c607a8
PatrickJGilmore <63202765+PatrickJGilmore@users.noreply.github.com>
2025-04-10 23:36:39 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { ViteSSG } from 'vite-ssg';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     ViteSSG({
-      // Define routes to prerender
       routes: [
         { path: '/' },
         { path: '/about' },
