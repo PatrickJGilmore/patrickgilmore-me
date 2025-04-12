70b2aecdf26d3de7a992bb67482220c24cba3018
PatrickJGilmore <63202765+PatrickJGilmore@users.noreply.github.com>
2025-04-11 22:35:34 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,27 @@
-// vite.config.ts import { defineConfig } from 'vite'; import react from '@vitejs/plugin-react-swc'; import path from 'path'; import ssr from 'vite-plugin-ssr/plugin';
+// vite.config.ts
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import ssr from 'vite-plugin-ssr/plugin';
 
-export default defineConfig({ plugins: [ react(), ssr() ], resolve: { alias: { '@': path.resolve(__dirname, './src') } }, build: { outDir: 'dist', cssCodeSplit: true, minify: 'terser', terserOptions: { compress: { drop_console: true, }, }, }, });
+export default defineConfig({
+  plugins: [
+    react(),
+    ssr()
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  build: {
+    outDir: 'dist',
+    cssCodeSplit: true,
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+      },
+    },
+  },
+});
